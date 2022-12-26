5b9cd76069e3102a63f6509b9f8fc34a67cf9bd5
Diego Ramirez <die35go45@gmail.com>
2022-12-25 22:36:43 -0500

    mejora de botones

--- js/modules/characters.js
+++ js/modules/characters.js
@@ -3,22 +3,24 @@ const characters = () => {
         const charactersCharacters = document.getElementById('charactersCharacters');
 
         data.forEach((element) => {
-            const { id, name, description, urlImage, urlRecipe} = element;
+            const { id, name, status, species, type, gender,origin, location, image} = element;
             charactersCharacters.innerHTML += `
-                <div class="col-md-4" daat-key=${id}>
+                <div class="col-md-4" key=${id}>
                     <div class="card bg-dark border border-2 border-light border-opacity-25 h-100 mx-auto"
                         style="width: min(100%, 18rem);">
                         <div class="card-header text-center">
                             ${name}
                         </div>
                         <div class="card-body">
-                            <img src="${urlImage}" class="card-img-top" alt="comida" width="256" height="144"
+                            <img src="${image}" class="card-img-top" alt="comida" width="256" height="144"
                             style="width: 100%; height: 192px; object-fit: cover; border-radius: 0.9rem">
-                            <p class="card-text mt-2">${description}</p>
+                            <h5 class="card-title mt-2">${name}</h5>
+                            <h6 class="card-subtitle mb-2 text-secondary">${type || 'Desconocido'}</h6>
+                            <p class="card-text">${status}</p>
                         </div>
-                        <div class="card-footer text-muted text-center">
-                            <a href="${urlRecipe}" class="btn btn-primary" target=" _blank"
-                                rel="noopener noreferrer">Receta</a>
+                        <div class="card-footer text-center">
+                            <a href="${image}" class="btn bg-gradient shadow border-top" style="background-color: #58ea27; border-radius: 1rem" target=" _blank"
+                                rel="noopener noreferrer">Descargar imagen</a>
                         </div> 
                     </div>
                 </div>
@@ -27,11 +29,16 @@ const characters = () => {
     };
 
     const fetchRead = async() => {   
-        const response = await fetch('https://rickandmortyapi.com/api/character?page=1')
-        const data = await response.json();
-        console.log(data);
-        // renderCharacters(data);         
-    };    
+        try {
+            const {data} = await axios.get('https://rickandmortyapi.com/api/character?page=1');
+            renderCharacters(data.results);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log('fetchRead');
+        }           
+    };     
+
     fetchRead();
 };
 
